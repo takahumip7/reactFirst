a4f6a854dca2f9786f73ec1b8740122a60cf5aef
takahumip7 <72800775+takahumip7@users.noreply.github.com>
2022-01-01 19:34:13 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { ColorfulMessage } from "./compornents/ColorfulMessage";
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは!</h1>
+      <ColorfulMessage color="blue">お元気ですか。</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です。</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>( ^ω^ )</p>}
+    </>
+  );
+};
+
+export default App;
---
+++ src/compornents/ColorfulMessage.jsx
@@ -0,0 +1,9 @@
+export const ColorfulMessage = (props) => {
+  const { color, children } = props;
+  const contentStyle = {
+    color,
+    fontSize: "18px"
+  };
+
+  return <p style={contentStyle}>{children}</p>;
+};
